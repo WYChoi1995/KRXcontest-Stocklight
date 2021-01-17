d3f7a71f695eae40bc58e6f2e71663aa2122bc07
WYChoi1995 <60026903+WYChoi1995@users.noreply.github.com>
2021-01-17 10:41:19 +0900

    Delete investAlretModel.py

--- MainRound/investAlretModel.py
+++
@@ -1,5 +0,0 @@
-class InvestAlertModel(object):
-    def __init__(self, tickers, trainSet: dict, testSet: dict):
-        self.tickers = tickers
-        self.trainSet = trainSet
-        self.testSet = testSet
