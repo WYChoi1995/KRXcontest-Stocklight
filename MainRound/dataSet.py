60d91cb4f4a9cc5dc610bf51c8cc596099b66690
WYChoi1995 <60026903+WYChoi1995@users.noreply.github.com>
2021-01-20 17:03:29 +0900

    Update dataSet.py

--- MainRound/dataSet.py
+++ MainRound/dataSet.py
@@ -2,8 +2,8 @@ from pandas import read_csv
 
 tickerData = read_csv("./csvData/firmList.csv", encoding="cp949")
 
-kospiCode= [ticker for ticker in tickerData.loc[tickerData["Market"] == "KOSPI", "Symbol"].zfill(6)]
-kosdaqCode= [ticker for ticker in tickerData.loc[tickerData["Market"] == "KOSDAQ", "Symbol"].zfill(6)]
+kospiCode= [ticker for ticker in tickerData.loc[tickerData["Market"] == "KOSPI", "Symbol"].str.zfill(6)]
+kosdaqCode= [ticker for ticker in tickerData.loc[tickerData["Market"] == "KOSDAQ", "Symbol"].str.zfill(6)]
                                                 
 processor = PreProcessor(kospiTickers=kospiCode, kosdaqTickers=kosdaqCode, startDate="2016-01-01", endDate="2020-12-30",
                          alertData=investAlertData, rollingWindow=15)
